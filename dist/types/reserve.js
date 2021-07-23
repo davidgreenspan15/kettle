1ab9ba36c168cfa84f60fae34bc3576a8a6c38a2
davidgreenspan15 <ramigreenspangmail.com>
2021-07-23 09:58:13 -0400

    added webpack i think we resolved

--- dist/types/reserve.js
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=reserve.js.map
---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.tsx',
+  target: 'node',
+  mode: 'development',
+  devServer: {
+    contentBase: './dist',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    publicPath: '/',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'source-map-loader',
+      },
+      {
+        test: /\.css$/,
+        loader: 'css-loader',
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|ico)$/,
+        exclude: /node_modules/,
+        use: ['file-loader?name=[name].[ext]'], // ?name=[name].[ext] is only necessary to preserve the original file name
+      },
+    ],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(
+        process.env.NODE_ENV || 'development'
+      ),
+      'process.env.REACT_APP_LOCAL': JSON.stringify(
+        process.env.REACT_APP_LOCAL || 'True'
+      ),
+    }),
+
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html'),
+      favicon: path.resolve(__dirname, 'public', 'manifest.json'),
+    }),
+    new MiniCssExtractPlugin({
+      filename: './src/App.css',
+    }),
+  ],
+};
