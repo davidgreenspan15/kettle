e399253bd086eb4f397e3357595b7f19e48ba90b
davidgreenspan15 <ramigreenspangmail.com>
2021-07-25 02:12:58 -0400

    another build attemtp 6

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 let express = require('express');
-const path = require('path');
 
 let app = express();
 app.use(express.static(__dirname + '/'));
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('build'));
   app.get('*', (req, res) => {
-    res.sendFile(path.join('build', 'index.html'));
+    res.sendFile('index.html', { root: 'build' });
   });
 }
 
