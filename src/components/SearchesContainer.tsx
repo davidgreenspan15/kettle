10f8df7cdcd7918198657e636e5f89fa21ec1af9
davidgreenspan15 <ramigreenspangmail.com>
2021-08-01 16:08:09 -0400

    trying out accordion style for searches

--- src/components/SearchesContainer.tsx
+++ src/components/SearchesContainer.tsx
@@ -8,6 +8,11 @@ import {
   Thead,
   Tr,
   useMediaQuery,
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
 } from '@chakra-ui/react';
 import moment from 'moment';
 import React from 'react';
@@ -18,35 +23,51 @@ const SearchesContainer: React.FC<{ t: Ticket }> = ({ t }) => {
   const moreThan600 = useMediaQuery('(min-width: 680px)');
   return (
     <Flex flexDirection="column" px={['0px', '25px']} pt="10px">
-      <Heading size="xs">Searches</Heading>
-      <Table size="sm">
-        <Thead>
-          <Tr>
-            <Th>Time</Th>
-            <Th>Status</Th>
-            <Th>Comments</Th>
-          </Tr>
-        </Thead>
-        {t?.searches?.map((s, idx) => {
-          return (
-            <Tbody key={idx}>
-              <Tr>
-                <Td fontSize={['10px', '12px', '14px']} py={[0, '5px']}>
-                  {moreThan600[0]
-                    ? moment(s.createdAt).format('MMMM Do YYYY, h:mm a')
-                    : moment(s.createdAt).format('MM/DD/YY, h:mm a')}
-                </Td>
-                <Td py={[0, '5px']} fontSize={['10px', '12px', '14px']}>
-                  {s.status}
-                </Td>
-                <Td py={[0, '5px']} fontSize={['10px', '12px', '14px']}>
-                  {s.comments}
-                </Td>
-              </Tr>
-            </Tbody>
-          );
-        })}
-      </Table>
+      <Accordion allowToggle>
+        <AccordionItem>
+          <AccordionButton>
+            <Flex
+              flexDirection="row"
+              justifyContent="space-between"
+              width="100%"
+            >
+              <Heading size="xs">Searches</Heading>
+              <Heading size="xs">{t.searches?.length}</Heading>
+            </Flex>
+            <AccordionIcon />
+          </AccordionButton>
+          <AccordionPanel pb={4}>
+            <Table size="sm">
+              <Thead>
+                <Tr>
+                  <Th>Time</Th>
+                  <Th>Status</Th>
+                  <Th>Comments</Th>
+                </Tr>
+              </Thead>
+              {t?.searches?.map((s, idx) => {
+                return (
+                  <Tbody key={idx}>
+                    <Tr>
+                      <Td fontSize={['10px', '12px', '14px']} py={[0, '5px']}>
+                        {moreThan600[0]
+                          ? moment(s.createdAt).format('MMMM Do YYYY, h:mm a')
+                          : moment(s.createdAt).format('MM/DD/YY, h:mm a')}
+                      </Td>
+                      <Td py={[0, '5px']} fontSize={['10px', '12px', '14px']}>
+                        {s.status}
+                      </Td>
+                      <Td py={[0, '5px']} fontSize={['10px', '12px', '14px']}>
+                        {s.comments}
+                      </Td>
+                    </Tr>
+                  </Tbody>
+                );
+              })}
+            </Table>
+          </AccordionPanel>
+        </AccordionItem>
+      </Accordion>
     </Flex>
   );
 };
